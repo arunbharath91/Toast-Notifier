60a1c062e57fa08843e6005bd4bfb579adeed54a
Arun Bharath <arunbharath91@gmail.com>
2020-03-14 23:06:07 +0530

    changes

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,7 +1,2 @@
 import '../style.scss';
-
-import { toaster } from './toaster';
-
-(document.querySelector('.btn') as HTMLElement).addEventListener('click', () => {
-  toaster.success();
-});
+import "./toaster-config";
---
+++ src/app/toaster-config.ts
@@ -0,0 +1,5 @@
+import { toaster } from "./toaster";
+
+(document.querySelector('.btn') as HTMLElement).addEventListener('click', () => {
+  toaster.success();
+});
